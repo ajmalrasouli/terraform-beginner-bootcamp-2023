570fbec03260aaec695b4ea47dab5a10a302b31d
AjmalRasouli <88502375+ajmalrasouli@users.noreply.github.com>
2023-10-03 21:47:16 +0100

    #36 upload assets using a for_each (#37)

--- main.tf
+++ main.tf
@@ -18,6 +18,7 @@ terraform {
 
 module "terrahouse_aws" {
   source = "./modules/terrahouse_aws"
+  assets_path = "/workspace/terraform-beginner-bootcamp-2023/public/assets/"
   user_uuid = var.user_uuid
   bucket_name = var.bucket_name
   index_html_filepath = var.index_html_filepath
